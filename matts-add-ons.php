5cc113cfee66abab095b85574515b3c4c7e02ea5
mattmcgiv <matt@antym.com>
2015-07-28 17:52:38 -0600

    js correctly enqueued after jquery

--- matts-add-ons.php
+++ matts-add-ons.php
@@ -13,10 +13,9 @@ Text Domain: antym-plugin
 */
 
 function mm_enqueue_scripts_styles() {
-  wp_enqueue_script('mmjs', '/wp-content/plugins/matts-add-ons/js/mmjs.js', '', true);
+  wp_enqueue_script('mmjs', plugins_url('js/mm.js', __FILE__), array('jquery'), true);
   wp_enqueue_style('basscss', '/wp-content/plugins/matts-add-ons/css/basscss.min.css');
   wp_enqueue_style('matts-style', '/wp-content/plugins/matts-add-ons/css/matts-style.css','basscss');
-  
 }
 
 add_action('wp_enqueue_scripts', 'mm_enqueue_scripts_styles');
